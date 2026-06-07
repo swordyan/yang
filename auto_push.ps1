380e9d4ecf4cc79885a1eb0a9ae6896b6a6565e6
your_github_username <your_email@example.com>
2026-06-07 18:29:21 +0800

    auto update all files at 2026-06-07 18:29:21

--- auto_push.ps1
+++ auto_push.ps1
@@ -1,4 +1,5 @@
 ﻿cd E:\my_daily_push
-git add knowledge_push_edition.md
-git commit -m "auto update at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+git pull origin master --no-edit
+git add .
+git commit -m "auto update all files at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 git push origin master
